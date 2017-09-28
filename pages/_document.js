45b9969f4238117ace1dc853fe48766d3c17e9f4
Lior Shefer <lior.shefer@wix.com>
2017-09-28 23:14:53 +0300

    more ssl warnings

--- pages/_document.js
+++ pages/_document.js
@@ -15,9 +15,8 @@ export default class MyDocument extends Document {
          <style>{`body { margin: 0 } html { font-family: Roboto, sans-serif; -webkit-font-smoothing: antialiased; }`}</style>
          <meta charSet='utf-8' />
          <meta name='viewport' content='initial-scale=1.0, width=device-width' />
-         <link href="https://fonts.googleapis.com/css?family=Roboto" rel="stylesheet" />
-         <link href="https://unpkg.com/tachyons@4.8.1/css/tachyons.min.css" rel="stylesheet" />
-         <link rel='stylesheet' type='text/css' href='http://ricostacruz.com/nprogress/nprogress.css' />
+         <link href="//unpkg.com/tachyons@4.8.1/css/tachyons.min.css" rel="stylesheet" />
+         <link rel='stylesheet' type='text/css' href='//ricostacruz.com/nprogress/nprogress.css' />
          <script src="/static/js/web3.min.js"></script>
        </Head>
        <body>
